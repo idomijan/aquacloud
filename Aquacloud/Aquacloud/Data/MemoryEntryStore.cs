d0ceb2d57f3133d4e0a7f0b2065d3c63a3646d5b
idomijan <idomijan1@gmail.com>
2019-03-06 11:39:14 +0100

    First commit, notes crud working

---
+++ Aquacloud/Aquacloud/Data/MemoryEntryStore.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aquacloud.Data
+{
+	class MemoryEntryStore : INoteEntryStore
+    {
+		private readonly Dictionary<string, NoteEntry> entries = new Dictionary<string, NoteEntry>();
+
+		public Task<IEnumerable<NoteEntry>> GetAllAsync()
+		{
+			IEnumerable<NoteEntry> result = entries.Values.ToList();
+			return Task.FromResult(result);
+		}
+
+		public Task AddAsync(NoteEntry entry)
+		{
+			entries.Add(entry.Id, entry);
+			return Task.CompletedTask;
+		}
+
+		public Task UpdateAsync(NoteEntry entry)
+		{
+			return Task.CompletedTask;
+		}
+
+		public Task DeleteAsync(NoteEntry entry)
+		{
+			entries.Remove(entry.Id);
+			return Task.CompletedTask;
+		}
+
+		public Task<NoteEntry> GetByIdAsync(string id)
+		{
+			NoteEntry entry = null;
+			entries.TryGetValue(id, out entry);
+			return Task.FromResult(entry);
+		}
+	}
+}
---
+++ Aquacloud/Aquacloud/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Aquacloud.Data;
+
+namespace Aquacloud
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			entries.ItemTapped += OnItemTapped;
+			newEntry.Completed += OnAddNewEntry;
+		}
+
+		//ADD NEW NOTE
+		private async void OnAddNewEntry(object sender, EventArgs e)
+		{
+			string text = newEntry.Text;
+			if (!string.IsNullOrEmpty(text)) {
+				NoteEntry item = new NoteEntry { Title = text };
+				await App.Entries.AddAsync(item);
+				await Navigation.PushAsync(new NoteEntryEditPage(item));
+				newEntry.Text = string.Empty;
+			}
+		}
+
+		private async void OnItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			NoteEntry item = e.Item as NoteEntry;
+			await Navigation.PushAsync(new NoteEntryEditPage(item));
+		}
+
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+			entries.ItemsSource = await App.Entries.GetAllAsync();
+		}
+	}
+}
